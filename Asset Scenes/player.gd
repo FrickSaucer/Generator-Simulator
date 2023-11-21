7fcec918964a7ab8e262ae8a80280177c4bbc2cd
Imlusman <hugonotten06@gmail.com>
2023-11-21 08:58:12 +0100

    h

---
+++ Asset Scenes/player.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+const RAY_LENGTH = 1000
+var Body = $CharacterBody3D
+
+func _physics_process(delta):
+	var space_state = get_world_3d().direct_space_state
+	var cam = $Camera3D
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
